83489bd6b0e01ec94262494f5a9c76cc71032a7d
kamalkrbh <kamal.kr.bh@gmail.com>
2023-01-13 12:45:45 +0100

    project init

---
+++ src/main/java/orca/stordis/backend/orca_backend/OrcaBackendApplication.java
@@ -0,0 +1,13 @@
+package orca.stordis.backend.orca_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orca/stordis/backend/orca_backend/OrcaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package orca.stordis.backend.orca_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
