b62ed4b742695f8bc9b2eab17534a59c3c197033
kamalkrbh <kamal.kr.bh@gmail.com>
2023-01-19 15:04:43 +0100

    remove unused imports

--- src/main/java/orca/stordis/backend/orca_backend/OrcaBackendApplication.java
+++ src/main/java/orca/stordis/backend/orca_backend/OrcaBackendApplication.java
@@ -1,25 +1,10 @@
 package orca.stordis.backend.orca_backend;
 
-import java.net.http.HttpHeaders;
-import java.security.cert.X509Certificate;
-import java.util.Base64;
 import java.util.logging.Logger;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-// import org.apache.http.impl.client.CloseableHttpClient;
-// import org.apache.http.impl.client.HttpClients;
 
 @SpringBootApplication
 public class OrcaBackendApplication {
