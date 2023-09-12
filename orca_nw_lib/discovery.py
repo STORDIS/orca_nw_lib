6eb4c949fd8658c0463f37c02e3f8ee170786a01
kamalkrbh <kamal.kr.bh@gmail.com>
2023-09-12 07:10:48 +0200

    Added doc string to functions.

--- orca_nw_lib/discovery.py
+++ orca_nw_lib/discovery.py
@@ -24,10 +24,33 @@ _logger = get_logging().getLogger(__name__)
 topology = {}
 
 
-def read_lldp_topo(ip):
+def read_lldp_topo(ip: str):
+    """
+    Starting from an IP address, Read LLDP table recursively 
+    untill all the connected devices are discovered.
+    Keeps the discovered devices in the `topology` dictionary.
+    Sample `topology` dictionary :
+    {<Device: 10.10.130.212>: [{'local_port': 'Ethernet0',
+                            'nbr_device': <Device: 10.10.130.210>,
+                            'nbr_port': 'Ethernet1'}]}
+    Args:
+        ip (str): The IP address of the device.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If there is an error while discovering the device.
+
+    Description:
+        This function reads the LLDP (Link Layer Discovery Protocol) topology of a device. It takes an IP address as input and creates a device graph object using the `create_device_graph_object` function. If the device is not already in the `topology` dictionary, it retrieves the LLDP neighbors of the device using the `get_lldp_neighbors` function. For each neighbor, it creates a neighbor device graph object and checks if the neighbor device has a management interface. If it does, it appends the neighbor device and its corresponding ports to a temporary array. The temporary array is then assigned to the `topology` dictionary with the device as the key. Finally, the function recursively calls itself for each neighbor device to discover their LLDP topology.
+
+        If an exception occurs during the execution of the function, an error message is logged using the `_logger` object.
+
+    """
     try:
         device = create_device_graph_object(ip)
-        if device not in topology.keys():
+        if device not in topology:
             nbrs = get_lldp_neighbors(ip)
             temp_arr = []
             for nbr in nbrs:
@@ -64,11 +87,21 @@ def insert_topology_in_db(topology):
 
 
 def discover_topology():
-    nw_to_discover= get_orca_config().get(network)
+    """
+    Discover the network topology.
+
+    This function discovers the network topology by querying the Orca configuration to obtain the network to be discovered.
+    It then iterates over each IP and calls the function `read_lldp_topo` to discover the LLDP topology of the device and its neighbors.
+
+    Parameters:
+        None
+
+    Returns:
+        bool: True if the network topology was successfully discovered and inserted into the database, False otherwise.
+    """
+    nw_to_discover = get_orca_config().get(network)
     _logger.info(
-        "Network Discovery Started using network provided {0}".format(
-            nw_to_discover
-        )
+        "Network Discovery Started using network provided {0}".format(nw_to_discover)
     )
     try:
         for ip_or_nw in nw_to_discover:
@@ -80,7 +113,7 @@ def discover_topology():
 
         _logger.info(
             "Discovered topology using network provided {0}: \n{1}".format(
-                get_orca_config().get(network), pprint.pformat(topology)
+                nw_to_discover, pprint.pformat(topology)
             )
         )
         _logger.info(f"Total devices discovered:{len(topology)}")
@@ -98,6 +131,16 @@ def discover_topology():
 
 
 def create_lldp_rel():
+    """
+    Create an LLDP relation using the global dictionary `topology`.
+    Function should be called after discovering the interfaces.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     _logger.info("Discovering LLDP relations.")
     create_lldp_relations_in_db(topology)
 
@@ -115,9 +158,9 @@ def discover_all():
     topology = {}
     if discover_topology():
         discover_interfaces()
+        create_lldp_rel()
         discover_port_groups()
         discover_vlan()
-        create_lldp_rel()
         discover_port_chnl()
         discover_mclag()
         discover_mclag_gw_macs()
