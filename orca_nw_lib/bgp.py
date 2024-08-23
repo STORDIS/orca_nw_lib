f8052d2767a95fd3df5be0c3d34cd5c1b7f3b9f7
rahul <rahul.s@metricdust.com>
2024-08-23 10:33:20 +0530

    adding new bgp configurations

--- orca_nw_lib/bgp.py
+++ orca_nw_lib/bgp.py
@@ -5,8 +5,8 @@ from .bgp_db import (
     get_bgp_global_with_vrf_from_db,
     get_remote_bgp_asn_from_db,
     get_bgp_neighbor_subinterfaces_from_db,
-    insert_bgp_global_af_list_in_db,
     insert_device_bgp_in_db,
+    get_bgp_global_af_from_db, get_bgp_global_af_network_from_db, get_bgp_global_af_aggregate_addr_from_db,
 )
 from .bgp_gnmi import (
     config_bgp_global_af_on_device,
@@ -19,17 +19,20 @@ from .bgp_gnmi import (
     del_bgp_global_from_device,
     get_bgp_global_af_list_from_device,
     get_bgp_global_list_from_device,
-    get_bgp_neighbors_from_device,
+    get_bgp_neighbors_from_device, del_bgp_global_af_from_device, config_bgp_global_af_network_on_device,
+    get_bgp_global_af_network_from_device, del_bgp_global_af_network_on_device,
+    config_bgp_global_af_aggregate_addr_on_device, del_bgp_global_af_aggregate_addr_on_device,
+    get_bgp_global_af_aggregate_addr_from_device, get_bgp_details_from_device,
 )
 from .device_db import get_device_db_obj
 
-from .graph_db_models import BGP, BGP_GLOBAL_AF
+from .graph_db_models import BGP, BGP_GLOBAL_AF, BGP_GLOBAL_AF_NETWORK, BGP_GLOBAL_AF_AGGREGATE_ADDR
 from .utils import get_logging
 
 _logger = get_logging().getLogger(__name__)
 
 
-def _create_bgp_graph_objects(device_ip: str) -> List[BGP]:
+def _create_bgp_graph_objects(device_ip: str) -> dict:
     """
     Creates BGP graph objects based on the given device IP.
 
@@ -39,17 +42,60 @@ def _create_bgp_graph_objects(device_ip: str) -> List[BGP]:
     Returns:
         List[BGP]: A list of BGP graph objects created based on the device's BGP configuration.
     """
+    bgp_details = get_bgp_details_from_device(device_ip).get("sonic-bgp-global:sonic-bgp-global", {})
 
-    global_list = get_bgp_global_list_from_device(device_ip)
-    bgp_global_list = []
+    global_list = bgp_details.get("BGP_GLOBALS", {})
+    bgp_global_list = {}
 
-    for bgp_config in global_list.get("sonic-bgp-global:BGP_GLOBALS_LIST") or []:
-        bgp = BGP(
+    for bgp_config in global_list.get("BGP_GLOBALS_LIST") or []:
+
+        # adding af
+        global_af_list = bgp_details.get("BGP_GLOBALS_AF", {}).get("BGP_GLOBALS_AF_LIST", {})
+        af_list = []
+        bgp_family = {}
+        for af in global_af_list:
+            af_list.append(
+                BGP_GLOBAL_AF(
+                    afi_safi=af.get("afi_safi"),
+                    vrf_name=af.get("vrf_name"),
+                    max_ebgp_paths=int(af.get("max_ebgp_paths")) if af.get("max_ebgp_paths") else None,
+                )
+            )
+        bgp_family["af"] = af_list
+
+        # adding af network
+        global_af_network_list = bgp_details.get("BGP_GLOBALS_AF_NETWORK", {}).get("BGP_GLOBALS_AF_NETWORK_LIST", {})
+        af_network_list = []
+        for af_network in global_af_network_list:
+            af_network_list.append(
+                BGP_GLOBAL_AF_NETWORK(
+                    afi_safi=af_network.get("afi_safi"),
+                    vrf_name=af_network.get("vrf_name"),
+                    ip_prefix=af_network.get("ip_prefix")
+                )
+            )
+        bgp_family["af_network"] = af_network_list
+
+        # adding af aggregate address
+        global_af_aggregate_addr_list = bgp_details.get("BGP_GLOBALS_AF_AGGREGATE_ADDR", {}).get(
+            "BGP_GLOBALS_AF_AGGREGATE_ADDR_LIST", {}
+        )
+        af_aggregate_addr_list = []
+        for af_aggregate_addr in global_af_aggregate_addr_list:
+            af_aggregate_addr_list.append(
+                BGP_GLOBAL_AF_AGGREGATE_ADDR(
+                    afi_safi=af_aggregate_addr.get("afi_safi"),
+                    vrf_name=af_aggregate_addr.get("vrf_name"),
+                    ip_prefix=af_aggregate_addr.get("ip_prefix")
+                )
+            )
+        bgp_family["af_aggregate_addr"] = af_aggregate_addr_list
+
+        bgp_global_list[BGP(
             local_asn=bgp_config.get("local_asn"),
             router_id=bgp_config.get("router_id"),
             vrf_name=bgp_config.get("vrf_name"),
-        )
-        bgp_global_list.append(bgp)
+        )] = bgp_family
 
     bgp_nbrs = get_bgp_neighbors_from_device(device_ip).get(
         "sonic-bgp-neighbor:sonic-bgp-neighbor", {}
@@ -73,19 +119,6 @@ def _create_bgp_graph_objects(device_ip: str) -> List[BGP]:
                 nbr_details_list.append(nbr)
         bgp.neighbor_prop = nbr_details_list
     return bgp_global_list
-
-
-def _create_bgp_global_af_graph_objects(device_ip: str) -> List[BGP_GLOBAL_AF]:
-    global_af_list = get_bgp_global_af_list_from_device(device_ip).get(
-        "sonic-bgp-global:BGP_GLOBALS_AF_LIST"
-    )
-
-    return [
-        BGP_GLOBAL_AF(
-            afi_safi=global_af.get("afi_safi"), vrf_name=global_af.get("vrf_name")
-        )
-        for global_af in global_af_list or []
-    ]
 
 
 def get_bgp_global(device_ip, vrf_name: str = None):
@@ -117,7 +150,7 @@ def get_bgp_global(device_ip, vrf_name: str = None):
 
 
 def config_bgp_global(
-    device_ip: str, local_asn: int, router_id: str, vrf_name: str = "default"
+        device_ip: str, local_asn: int, router_id: str, vrf_name: str = "default"
 ) -> None:
     """
     Configures the BGP global settings on the specified device,
@@ -223,7 +256,7 @@ def get_neighbour_bgp(device_ip: str, asn: int) -> List[dict]:
 
 
 def config_bgp_neighbors(
-    device_ip: str, remote_asn: int, neighbor_ip: str, remote_vrf: str
+        device_ip: str, remote_asn: int, neighbor_ip: str, remote_vrf: str
 ):
     """
     Configures BGP neighbors on a device.
@@ -296,41 +329,12 @@ def discover_bgp(device_ip: str = None):
     create_bgp_peer_link_rel()
 
 
-def discover_bgp_af_global(device_ip: str = None):
-    """
-    Discovers the BGP Global AF List.
-    Retrieves the list of devices from the database and for each device,
-    discovers BGP and inserts the BGP information into the database.
-    Lastly, creates the BGP peer link relationships.
-
-    Parameters:
-    None
-
-    Returns:
-    None
-    """
-
-    _logger.info("Discovering BGP Global AF List.")
-    devices = [get_device_db_obj(device_ip)] if device_ip else get_device_db_obj()
-    for device in devices:
-        try:
-            _logger.info(f"Discovering BGP Global AF List on device {device.mgt_ip}.")
-            insert_bgp_global_af_list_in_db(
-                device, _create_bgp_global_af_graph_objects(device.mgt_ip)
-            )
-        except Exception as e:
-            _logger.error(
-                f"BGP Global AF List Discovery Failed on device {device.mgt_ip}, Reason: {e}"
-            )
-            raise
-
-
 def config_bgp_neighbor_af(
-    device_ip: str,
-    afi_safi: str,
-    neighbor_ip: str,
-    vrf: str,
-    admin_status: bool = True,
+        device_ip: str,
+        afi_safi: str,
+        neighbor_ip: str,
+        vrf: str,
+        admin_status: bool = True,
 ):
     """
     Configure BGP neighbor address family.
@@ -397,7 +401,7 @@ def get_bgp_global_af_list(device_ip: str) -> List[dict]:
     ]
 
 
-def config_bgp_global_af(device_ip: str, afi_safi: str, vrf_name: str = "default"):
+def config_bgp_global_af(device_ip: str, afi_safi: str, vrf_name: str = "default", max_ebgp_paths: int = None):
     """
     Configures the BGP global address family (AF) on a device.
 
@@ -405,19 +409,20 @@ def config_bgp_global_af(device_ip: str, afi_safi: str, vrf_name: str = "default
         device_ip (str): The IP address of the device.
         afi_safi (str): The AF and SAFI (Address Family and Subsequent Address Family Identifier) to configure.
         vrf_name (str, optional): The name of the VRF (Virtual Routing and Forwarding) instance. Defaults to "default".
+        max_ebgp_paths (int, optional): The maximum number of EBGP paths. Defaults to None.
 
     Returns:
         None
     """
     try:
-        config_bgp_global_af_on_device(device_ip, afi_safi, vrf_name)
+        config_bgp_global_af_on_device(device_ip, afi_safi, vrf_name, max_ebgp_paths)
     except Exception as e:
         _logger.error(
             f"Failed to configure AF {afi_safi} on BGP on device {device_ip}, Reason: {e}."
         )
         raise
     finally:
-        discover_bgp_af_global()
+        discover_bgp(device_ip=device_ip)
 
 
 def del_bgp_global_af_all(device_ip: str):
@@ -438,4 +443,209 @@ def del_bgp_global_af_all(device_ip: str):
         )
         raise
     finally:
-        discover_bgp_af_global()
+        discover_bgp()
+
+
+def del_bgp_global_af(device_ip: str, vrf_name: str, afi_safi: str = None):
+    """
+    Deletes all BGP global AF from all devices.
+
+    Args:
+        device_ip (str): The IP address of the device.
+        vrf_name (str): The name of the VRF (Virtual Routing and Forwarding) instance.
+        afi_safi (str, optional): The address family identifier. Defaults to None.
+    Returns:
+        None
+    """
+    try:
+        del_bgp_global_af_from_device(device_ip, vrf_name, afi_safi)
+    except Exception as e:
+        _logger.error(
+            f"Failed to delete AF from BGP on device {device_ip}, Reason: {e}."
+        )
+        raise
+    finally:
+        discover_bgp(device_ip=device_ip)
+
+
+def get_bgp_global_af(device_ip, asn: int, afi_safi: str = None):
+    """
+    Retrieve a list of BGP global AF dictionaries for a given device IP.
+
+    Args:
+        device_ip (str): The IP address of the device.
+        asn (int): The autonomous system number.
+        afi_safi (str, optional): The address family identifier. Defaults to None.
+
+    Returns:
+        list or dict: A list of dictionaries representing the BGP global AF properties.
+    """
+    bgp_af = get_bgp_global_af_from_db(device_ip, asn, afi_safi)
+    if isinstance(bgp_af, list):
+        return [i.__properties__ for i in bgp_af] if bgp_af else None
+    else:
+        return bgp_af.__properties__ if bgp_af else None
+
+
+def config_bgp_global_af_network(device_ip: str, afi_safi: str, ip_prefix: str, vrf_name: str = "default"):
+    """
+    Configures the BGP global address family (AF) network on a device.
+
+    Args:
+        device_ip (str): The IP address of the device.
+        afi_safi (str): The AF and SAFI (Address Family and Subsequent Address Family Identifier) to configure.
+        ip_prefix (str): The IP address and prefix length.
+        vrf_name (str, optional): The name of the VRF (Virtual Routing and Forwarding) instance. Defaults to "default".
+
+    Raises:
+        Exception: If there is an error while configuring the AF network on the device.
+
+    Returns:
+        None
+    """
+    try:
+        config_bgp_global_af_network_on_device(
+            device_ip=device_ip, vrf_name=vrf_name, afi_safi=afi_safi, ip_prefix=ip_prefix
+        )
+    except Exception as e:
+        _logger.error(
+            f"Failed to configure AF Network {afi_safi} on BGP on device {device_ip}, Reason: {e}."
+        )
+        raise
+    finally:
+        discover_bgp(device_ip=device_ip)
+
+
+def get_bgp_global_af_network(device_ip, asn: int, afi_safi: str = None):
+    """
+    Retrieves the BGP global address family network configuration from the database for a given device IP, ASN, and AFI/SAFI.
+
+    Args:
+        device_ip (str): The IP address of the device.
+        asn (int): The Autonomous System Number (ASN) of the BGP.
+        afi_safi (str, optional): The address family identifier (AFI) and sub-address family identifier (SAFI)
+            combination. Defaults to None.
+
+    Returns:
+        list[dict] or dict or None: A list of dictionaries representing the BGP global address family network
+        configuration, or a single dictionary if afi_safi is specified, or None if no configuration is found.
+    """
+    bgp_af_network = get_bgp_global_af_network_from_db(device_ip, asn, afi_safi)
+    if isinstance(bgp_af_network, list):
+        return [i.__properties__ for i in bgp_af_network] if bgp_af_network else None
+    else:
+        return bgp_af_network.__properties__ if bgp_af_network else None
+
+
+def del_bgp_global_af_network(device_ip: str, afi_safi: str, ip_prefix: str, vrf_name: str = "default"):
+    """
+    Deletes the BGP global address family network configuration from a device.
+
+    Args:
+        device_ip (str): The IP address of the device.
+        afi_safi (str): The address family identifier and sub-address family identifier combination.
+        ip_prefix (str): The IP address and prefix length.
+        vrf_name (str, optional): The name of the VRF instance. Defaults to "default".
+
+    Raises:
+        Exception: If there is an error while deleting the AF network on the device.
+
+    Returns:
+        None
+    """
+    try:
+        del_bgp_global_af_network_on_device(
+            device_ip=device_ip,
+            vrf_name=vrf_name,
+            afi_safi=afi_safi,
+            ip_prefix=ip_prefix
+        )
+    except Exception as e:
+        _logger.error(
+            f"Failed to delete AF Network {afi_safi} on BGP on device {device_ip}, Reason: {e}."
+        )
+        raise
+    finally:
+        discover_bgp(device_ip=device_ip)
+
+
+def config_bgp_global_af_aggregate_addr(device_ip: str, afi_safi: str, ip_prefix: str, vrf_name: str = "default"):
+    """
+    Configures the BGP global address family (AF) aggregate address on a device.
+
+    Args:
+        device_ip (str): The IP address of the device.
+        afi_safi (str): The AF and SAFI (Address Family and Subsequent Address Family Identifier) to configure.
+        ip_prefix (str): The IP address and prefix length.
+        vrf_name (str, optional): The name of the VRF (Virtual Routing and Forwarding) instance. Defaults to "default".
+
+    Raises:
+        Exception: If there is an error while configuring the AF network on the device.
+
+    Returns:
+        None
+    """
+    try:
+        config_bgp_global_af_aggregate_addr_on_device(
+            device_ip=device_ip, vrf_name=vrf_name, afi_safi=afi_safi, ip_prefix=ip_prefix
+        )
+    except Exception as e:
+        _logger.error(
+            f"Failed to configure AF Aggregate Address {afi_safi} on BGP on device {device_ip}, Reason: {e}."
+        )
+        raise
+    finally:
+        discover_bgp(device_ip=device_ip)
+
+
+def get_bgp_global_af_aggregate_addr(device_ip, asn: int, afi_safi: str = None):
+    """
+    Retrieves the BGP global address family aggregate address configuration from the database for a given device IP, ASN, and AFI/SAFI.
+
+    Args:
+        device_ip (str): The IP address of the device.
+        asn (int): The Autonomous System Number (ASN) of the BGP.
+        afi_safi (str, optional): The address family identifier (AFI) and sub-address family identifier (SAFI)
+            combination. Defaults to None.
+
+    Returns:
+        list[dict] | dict | None: A list of dictionaries representing the BGP global address family aggregate address network
+        configuration, or a single dictionary if afi_safi is specified, or None if no configuration is found.
+    """
+    bgp_af_aggregate_addr = get_bgp_global_af_aggregate_addr_from_db(device_ip, asn, afi_safi)
+    if isinstance(bgp_af_aggregate_addr, list):
+        return [i.__properties__ for i in bgp_af_aggregate_addr] if bgp_af_aggregate_addr else None
+    else:
+        return bgp_af_aggregate_addr.__properties__ if bgp_af_aggregate_addr else None
+
+
+def del_bgp_global_af_aggregate_addr(device_ip: str, afi_safi: str, ip_prefix: str, vrf_name: str = "default"):
+    """
+    Deletes the BGP global address family (AF) aggregate address configuration on a device.
+
+    Args:
+        device_ip (str): The IP address of the device.
+        afi_safi (str): The AF and SAFI (Address Family and Subsequent Address Family Identifier) to delete.
+        ip_prefix (str): The IP address and prefix length.
+        vrf_name (str, optional): The name of the VRF (Virtual Routing and Forwarding) in which the AF is configured. Defaults to "default".
+
+    Raises:
+        Exception: If there is an error while deleting the AF network on the device.
+
+    Returns:
+        None
+    """
+    try:
+        del_bgp_global_af_aggregate_addr_on_device(
+            device_ip=device_ip,
+            vrf_name=vrf_name,
+            afi_safi=afi_safi,
+            ip_prefix=ip_prefix
+        )
+    except Exception as e:
+        _logger.error(
+            f"Failed to delete AF Aggregate Address {afi_safi} on BGP on device {device_ip}, Reason: {e}."
+        )
+        raise
+    finally:
+        discover_bgp(device_ip=device_ip)
