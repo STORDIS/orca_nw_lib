43238033b333480643f26d2fce5dab78f571b638
ramya6737 <ramya.r@metricdust.com>
2024-12-05 19:47:16 +0530

    added influxdb code for mclag

--- orca_nw_lib/mclag_influxdb.py
+++ orca_nw_lib/mclag_influxdb.py
@@ -6,33 +6,28 @@ from orca_nw_lib.influxdb_utils import create_point, write_to_influx
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def insert_mclag_info_in_influxdb(device: Device, mclag_to_intfc_list):
-    """
-    Retrieves discovered port chanel data and inserts into influx DB.
-   
-    Args:
-        device (Device): Object to type Device.
-        mclag_to_intfc_list (dict): Dictionary of key value pairs.
-    """
+def insert_mclag_info_in_influxdb(device_ip: str, mclag_to_intfc_list: dict):
     try:
-        point = create_point("discovered_mclag")
-        for mclag in mclag_to_intfc_list.items() or []:
-            point_tag = point.tag("domain_id", mclag.domain_id)    
-            point_tag.field("domain_id", mclag.domain_id)
-            point_tag.field("keepalive_interval", mclag.keepalive_interval)
-            point_tag.field("mclag_sys_mac", mclag.mclag_sys_mac)
-            point_tag.field("peer_addr", mclag.peer_addr)
-            point_tag.field("peer_link", mclag.peer_link)
-            point_tag.field("session_timeout", mclag.session_timeout)
-            point_tag.field("source_address", mclag.source_address)
-            point_tag.field("oper_status", mclag.oper_status)
-            point_tag.field("role", mclag.role)
-            point_tag.field("system_mac", mclag.system_mac)
-            point_tag.field("delay_restore", mclag.delay_restore)
-            point_tag.field("session_vrf", mclag.session_vrf)
-            point_tag.field("fast_convergence", mclag.fast_convergence)
-           
+        print("mclag_to_intfc_list", mclag_to_intfc_list.keys())
+        for mclag_obj, interfaces in mclag_to_intfc_list.items() or []:
+            print("mclag_obj", mclag_obj)
+            point = create_point("discovered_mclag")
+            mclag_point = point.tag("device_ip", device_ip)
+            mclag_point.field("domain-id", mclag_obj.domain_id)
+            mclag_point.field("keepalive_interval", mclag_obj.keepalive_interval)
+            mclag_point.field("mclag_sys_mac", mclag_obj.mclag_sys_mac)
+            mclag_point.field("peer_addr", mclag_obj.peer_addr)
+            mclag_point.field("peer_link", mclag_obj.peer_link)
+            mclag_point.field("session_timeout", mclag_obj.session_timeout)
+            mclag_point.field("source_address", mclag_obj.source_address)
+            mclag_point.field("oper_status", mclag_obj.oper_status)
+            mclag_point.field("role", mclag_obj.role)
+            mclag_point.field("system_mac", mclag_obj.system_mac)
+            mclag_point.field("delay_restore", mclag_obj.delay_restore)
+            mclag_point.field("session_vrf", mclag_obj.session_vrf)
+            mclag_point.field("fast_convergence", mclag_obj.fast_convergence)
+            logger.debug(f"Point to write: {point}")
             write_to_influx(point=point)
-            logger.info("Metrics pushed to InfluxDB successfully.")
+        logger.info("Metrics pushed to InfluxDB successfully.")
     except Exception as e:
-        logger.error(f"Error instering in influxdb: {e}")
+        logger.error(f"Error inserting in InfluxDB: {e}", exc_info=True)
